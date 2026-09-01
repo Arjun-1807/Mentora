b1ca4c2e4fa6f048a581d15e99eca8532a5cbf3d
Sachin-0001 <168210869+Sachin-0001@users.noreply.github.com>
2026-09-02 00:35:07 +0530

    Remove legacy ad hoc UI components replaced by shadcn
    Signed-off-by: Sachin-0001 <168210869+Sachin-0001@users.noreply.github.com>

--- mentora-frontend/src/components/Card.js
+++
@@ -1,9 +0,0 @@
-export default function Card({ children, className = "" }) {
-  return (
-    <div
-      className={`rounded-2xl bg-navy-800 border border-white/10 shadow-card p-6 ${className}`}
-    >
-      {children}
-    </div>
-  );
-}
